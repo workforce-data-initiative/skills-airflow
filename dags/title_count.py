7eb88cd1e971b4f406dbbdd0055b41fdd5d73cbf
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-06-14 15:17:56 -0500

    Include new aggregations/interface in GeoTitleCount
    - Include FreetextSkillExtractor in TitleCount DAG
    - Include occupation Classifiers in TitleCount DAG
    - Convert title cleaner task to work on raw job postings since new fields are no longer computable based on first round aggregates alone

--- dags/title_count.py
+++ dags/title_count.py
@@ -1,21 +1,24 @@
 """Takes common schema job listings and performs
 title extraction, cleaning, and aggregation
 """
-import csv
 import logging
 import os
-import pandas as pd
+from collections import OrderedDict
 
 from airflow.hooks import S3Hook
 from airflow.operators import BaseOperator
 
 from skills_utils.time import datetime_to_quarter
 from skills_utils.s3 import upload
-from skills_utils.fs import check_create_folder
 from skills_ml.datasets import job_postings
+from skills_ml.algorithms.aggregators import\
+    CountAggregator, SkillAggregator, SocCodeAggregator
 from skills_ml.algorithms.aggregators.title import GeoTitleAggregator
-from skills_ml.algorithms.jobtitle_cleaner.clean import JobTitleStringClean, aggregate
+from skills_ml.algorithms.corpus_creators.basic import SimpleCorpusCreator
+from skills_ml.algorithms.jobtitle_cleaner.clean import JobTitleStringClean
 from skills_ml.algorithms.string_cleaners import NLPTransforms
+from skills_ml.algorithms.skill_extractors.freetext import FreetextSkillExtractor
+from skills_ml.algorithms.occupation_classifiers.classifiers import Classifier
 from config import config
 
 from utils.dags import QuarterlySubDAG
@@ -49,31 +52,49 @@ def define_title_counts(main_dag_name):
                 quarter,
                 config['job_postings']['s3_path']
             )
+            skills_filename = '{}/skills_master_table.tsv'\
+                .format(output_folder)
             title_cleaner = NLPTransforms().title_phase_one
-            counts, title_rollup = GeoTitleAggregator(title_cleaner=title_cleaner)\
-                .counts(job_postings_generator)
-
-            total_counts = 0
-            check_create_folder(count_filename)
-            with open(count_filename, 'w') as count_file:
-                count_writer = csv.writer(count_file, delimiter=',')
-                for key, count in counts.items():
-                    geo, title = key
-                    total_counts += count
-                    count_writer.writerow([geo, title, count])
-
-            rollup_counts = 0
-            check_create_folder(rollup_filename)
-            with open(rollup_filename, 'w') as rollup_file:
-                rollup_writer = csv.writer(rollup_file, delimiter=',')
-                for title, count in title_rollup.items():
-                    rollup_counts += count
-                    rollup_writer.writerow([title, count])
+            corpus_creator = SimpleCorpusCreator()
+            common_classifier = Classifier(
+                s3_conn=s3_conn,
+                classifier_id='ann_0614',
+                classify_kwargs={'mode': 'common'}
+            )
+            top_classifier = Classifier(
+                s3_conn=s3_conn,
+                classifier=common_classifier.classifier,
+                classifier_id='ann_0614',
+                classify_kwargs={'mode': 'top'}
+            )
+            job_aggregators = OrderedDict()
+            job_aggregators['counts'] = CountAggregator()
+            job_aggregators['skills'] = SkillAggregator(
+                corpus_creator=corpus_creator,
+                skill_extractor=FreetextSkillExtractor(
+                    skills_filename=skills_filename
+                )
+            )
+            job_aggregators['soc_code_common'] = SocCodeAggregator(
+                corpus_creator=corpus_creator,
+                occupation_classifier=common_classifier
+            )
+            job_aggregators['soc_code_top'] = SocCodeAggregator(
+                corpus_creator=corpus_creator,
+                occupation_classifier=top_classifier
+            )
+            aggregator = GeoTitleAggregator(
+                job_aggregators=job_aggregators,
+                title_cleaner=title_cleaner
+            )
+            aggregator.process_postings(job_postings_generator)
+            aggregator.save_counts(count_filename)
+            aggregator.save_rollup(rollup_filename)
 
             logging.info(
                 'Found %s count rows and %s title rollup rows for %s',
-                total_counts,
-                rollup_counts,
+                len(aggregator.group_values.keys()),
+                len(aggregator.rollup.keys()),
                 quarter,
             )
             upload(
@@ -95,106 +116,95 @@ def define_title_counts(main_dag_name):
 
     class JobTitleCleanOperator(BaseOperator):
         def execute(self, context):
+
             s3_conn = S3Hook().get_conn()
             quarter = datetime_to_quarter(context['execution_date'])
 
-            cleaned_count_filename = '{}/{}/{}.csv'.format(
+            count_filename = '{}/{}/{}.csv'.format(
                 output_folder,
                 config['output_tables']['cleaned_geo_title_count_dir'],
                 quarter
             )
-
-            cleaned_rollup_filename = '{}/{}/{}.csv'.format(
+            rollup_filename = '{}/{}/{}.csv'.format(
                 output_folder,
                 config['output_tables']['cleaned_title_count_dir'],
                 quarter
             )
 
-            count_filename = '{}/{}/{}.csv'.format(
-                output_folder,
-                config['output_tables']['geo_title_count_dir'],
-                quarter
+            job_postings_generator = job_postings(
+                s3_conn,
+                quarter,
+                config['job_postings']['s3_path']
+            )
+            skills_filename = '{}/skills_master_table.tsv'\
+                .format(output_folder)
+
+            first_phase_cleaner = NLPTransforms().title_phase_one
+            second_phase_cleaner = JobTitleStringClean().clean_title
+
+            def two_phases(title):
+                return second_phase_cleaner(first_phase_cleaner(title))
+
+            corpus_creator = SimpleCorpusCreator()
+            common_classifier = Classifier(
+                s3_conn=s3_conn,
+                classifier_id='ann_0614',
+                classify_kwargs={'mode': 'common'}
+            )
+            top_classifier = Classifier(
+                s3_conn=s3_conn,
+                classifier=common_classifier.classifier,
+                classifier_id='ann_0614',
+                classify_kwargs={'mode': 'top'}
+            )
+            job_aggregators = OrderedDict()
+            job_aggregators['counts'] = CountAggregator()
+            job_aggregators['skills'] = SkillAggregator(
+                corpus_creator=corpus_creator,
+                skill_extractor=FreetextSkillExtractor(
+                    skills_filename=skills_filename
+                )
+            )
+            job_aggregators['soc_code_common'] = SocCodeAggregator(
+                corpus_creator=corpus_creator,
+                occupation_classifier=common_classifier
+            )
+            job_aggregators['soc_code_top'] = SocCodeAggregator(
+                corpus_creator=corpus_creator,
+                occupation_classifier=top_classifier
+            )
+            aggregator = GeoTitleAggregator(
+                job_aggregators=job_aggregators,
+                title_cleaner=two_phases
+            )
+            aggregator.process_postings(job_postings_generator)
+            aggregator.save_counts(count_filename)
+            aggregator.save_rollup(rollup_filename)
+
+            logging.info(
+                'Found %s count rows and %s title rollup rows for %s',
+                len(aggregator.group_values.keys()),
+                len(aggregator.rollup.keys()),
+                quarter,
+            )
+            upload(
+                s3_conn,
+                count_filename,
+                '{}/{}'.format(
+                    config['output_tables']['s3_path'],
+                    config['output_tables']['cleaned_geo_title_count_dir']
+                )
+            )
+            upload(
+                s3_conn,
+                rollup_filename,
+                '{}/{}'.format(
+                    config['output_tables']['s3_path'],
+                    config['output_tables']['cleaned_title_count_dir']
+                )
             )
 
-            rollup_filename = '{}/{}/{}.csv'.format(
-                output_folder,
-                config['output_tables']['title_count_dir'],
-                quarter
-            )
-
-
-            if os.stat(count_filename).st_size > 0:
-                logging.info('Cleaning and aggregating geo job titles on %s', quarter)
-                geo_title_count_df = pd.read_csv(count_filename, header=None)
-                geo_title_count_df.columns = ['geo', 'title', 'count']
-                cleaned_geo_title_count_df = JobTitleStringClean().clean(geo_title_count_df)
-                agg_cleaned_geo_title_count_df = aggregate(cleaned_geo_title_count_df, ['geo', 'title'])
-
-                total_counts = 0
-                check_create_folder(cleaned_count_filename)
-                with open(cleaned_count_filename, 'w') as count_file:
-                    clean_geo_writer = csv.writer(count_file, delimiter=',')
-                    for idx, row in agg_cleaned_geo_title_count_df.iterrows():
-                        total_counts += row['count']
-                        clean_geo_writer.writerow([row['geo'], row['title'], row['count']])
-
-                logging.info(
-                    'Found %s geo title rows for %s',
-                    total_counts,
-                    quarter,
-                )
-            else:
-                logging.warning(
-                    'Geo Title Count file %s not found',
-                    count_filename
-                )
-                upload(
-                    s3_conn,
-                    cleaned_count_filename,
-                    '{}/{}'.format(
-                        config['output_tables']['s3_path'],
-                        config['output_tables']['cleaned_geo_title_count_dir']
-                    )
-                )
-
-            if os.stat(rollup_filename).st_size > 0:
-                logging.info('Cleaning and aggregating job titles on %s', quarter)
-                title_count_df = pd.read_csv(rollup_filename)
-                title_count_df.columns = ['title', 'count']
-                cleaned_title_count_df = JobTitleStringClean().clean(title_count_df)
-                agg_cleaned_title_count_df = aggregate(cleaned_title_count_df, ['title'])
-
-                rollup_counts = 0
-                check_create_folder(cleaned_rollup_filename)
-                with open(cleaned_rollup_filename, 'w') as count_file:
-                    clean_writer = csv.writer(count_file, delimiter=',')
-                    for idx, row in agg_cleaned_title_count_df.iterrows():
-                        rollup_counts += row['count']
-                        clean_writer.writerow([row['title'], row['count']])
-
-                logging.info(
-                    'Found %s title rollup rows for %s',
-                    rollup_counts,
-                    quarter,
-                )
-
-            else:
-                logging.warning(
-                    'Job title rollup file %s not found',
-                    rollup_filename
-                )
-                upload(
-                    s3_conn,
-                    cleaned_rollup_filename,
-                    '{}/{}'.format(
-                        config['output_tables']['s3_path'],
-                        config['output_tables']['cleaned_title_count_dir']
-                    )
-                )
-
-    jobtitle_clean = JobTitleCleanOperator(task_id='clean_title_count', dag=dag)
-    geo_count = GeoTitleCountOperator(task_id='geo_title_count', dag=dag)
-
-    jobtitle_clean.set_upstream(geo_count)
+    GeoTitleCountOperator(task_id='clean_phase_one_geo_count', dag=dag)
+    JobTitleCleanOperator(task_id='clean_phase_two_geo_count', dag=dag)
 
     return dag
